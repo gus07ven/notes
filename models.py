e6b10b05f4ff44a7e2610229ba4f0dec9eafa957
Gustavo Arismendi <gustavoarismendi@hotmail.com>
2019-11-11 15:20:49 -0600

    Implement create and read notes

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from mistune import markdown
 
 
 db = SQLAlchemy()
@@ -13,11 +14,15 @@ class User(db.Model):
     notes = db.relationship('Note', backref='author', lazy=True)
 
 
-class Notes(db.Model):
+class Note(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200))
     body = db.Column(db.Text)
     created_at = db.Column(db.DateTime, server_default=db.func.now())
     updated_at = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    @property
+    def body_html(self):
+        return markdown(self.body)
 
